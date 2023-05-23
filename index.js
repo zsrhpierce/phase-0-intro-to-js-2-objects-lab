61c7b4640c0e4fc9cd458ea1bfe064af9a1b4323
Cathy Pierce <zsrhpierce@gmail.com>
2023-05-22 18:39:21 -0600

    Modifying Objects

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+
+const employee = {};
+
+employee;
+employee.name = 'Cathy Pierce';
+employee['streetAdress'] = '11660 So Olive Berry';
+employee;
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+    newObj[key] = value;
+    return newObj;
+}
+const updateEmployee = updateEmployeeWithKeyAndValue(employee, "Haley", "123 Main");
+updateEmployee;
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+const destroyEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, "Zachary", "1234 Wisteria");
+destroyEmployee;
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key]
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    delete employee[key]
+    return employee;
+}
+
+
